32781be290008efbe61de6e613c8d1b961285e14
anmolsharma1214 <60529999+anmolsharma1214@users.noreply.github.com>
2020-07-29 20:50:51 -0400

    Assignment 4

---
+++ Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int ReleasedYear { get; set; }
+        public int Length { get; set; }
+        public int Rating { get; set; }
+
+        
+    }
+}
